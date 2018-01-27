1edf330f181bf405b583ceffcbd9c4a506c06231
Evan Brooks <brooks.ev@gmail.com>
2018-01-27 14:27:24 -0800

    Initial Commit

---
+++ walk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  walk
+//
+//  Created by Evan Brooks on 1/27/18.
+//  Copyright © 2018 Evan Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
